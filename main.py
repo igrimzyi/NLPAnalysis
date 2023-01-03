d8b1a04afa3b78377d90ff8c6147e8d46b9f7895
igrimzyi <1isaiahofficial1@gmail.com>
2023-01-02 23:40:14 -0800

    push

--- main.py
+++ main.py
@@ -4,14 +4,9 @@ import matplotlib.pyplot as plt;
 import seaborn as sns;
 
 import nltk 
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-nltk.download('vader_lexicon')
 
-from nltk.sentiment import SentimentIntensityAnalyzer;
-from tqdm.notebook import tqdm
+
+
 
 plt.style.use('ggplot')
 
@@ -21,20 +16,18 @@ df = pd.read_csv('./Reviews.csv')
 
 df.head()
 
-ax = df['Score'] \
-        .value_counts() \
-        .sort_index() \
-        .plot(kind='bar', title='Count by stars', figsize=(10,5));
+ax = df['Score'].value_counts().sort_index() \
+    .plot(kind='bar', 
+          title = 'Count of Reviews by Stars',
+          figsize=(10,5))
+ax.set_xlabel('Review Stars')
 
-ax.set_xlabel('Review Stars');
 
-
-plt.show();
+# plt.show();
 
 
 example = df['Text'][50];
 
-print(example);
 
 tokens = nltk.word_tokenize(example);
 tokens[:10]
@@ -44,14 +37,36 @@ nltk.pos_tag(tokens);
 tagged = nltk.pos_tag(tokens);
 tagged[:10];
 
-entities = nltk.chunk.ne_chunk(tagged);
-
+entities = nltk.chunk.ne_chunk(tagged)
 entities.pprint()
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+from tqdm import tqdm
 
 sia = SentimentIntensityAnalyzer()
 
-print(sia.polarity_scores(example))
+sia.polarity_scores(example);
 
+# running the polarity score on entire dataset
+res = {}
 for i, row in tqdm(df.iterrows(), total=len(df)):
-    text = row['Text'];
+    text = row['Text']
     myid = row['Id']
+    res[myid] = sia.polarity_scores(text)
+
+
+
+vaders = pd.DataFrame(res).T;
+
+vaders = vaders.reset_index().rename(columns={'index':'Id'});
+
+vaders = vaders.merge(df, how='left');
+
+# Vaders sentiment score and metadata
+vaders.head()
+
+ax = sns.barplot(data=vaders, x='Score', y='compound');
+ax.set_title('Comp scores')
+plt.show()
+
+    
